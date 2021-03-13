0bfab87c495df31b39970793863973b127ae6bbe
yashagrawal39 <yash21296@gmail.com>
2021-03-13 23:41:28 +0530

    design

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.openSitebararea').click(function(){
+        $('#navSidebar').toggleClass('opensidebar');
+        $('#main').toggleClass('mainboxarea');
+    });
+});
